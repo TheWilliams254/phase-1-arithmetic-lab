0f2d854e11d53087628c56f12ef61288cf32a1ec
william <williamwambugu663@gmail.com>
2025-03-04 18:14:01 +0300

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+//multiplication
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+//random number generator
+const random = Math.floor(Math.random() * 10) + 1;
+//remainder
+const num3 = 20;
+const num4 = 5;
+const mod = num3/num4;
+ // math.max
+ const max = Math.max(12, 16, 17, 19, 20);
